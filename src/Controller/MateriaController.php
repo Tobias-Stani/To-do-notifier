e145c699ef3111a8c204b39e90459e81e7e6f571
Tobi-Stani <stanislavskytobias@gmail.com>
2024-10-23 21:56:11 -0300

    agregado cambio a la vista

--- src/Controller/MateriaController.php
+++ src/Controller/MateriaController.php
@@ -54,6 +54,10 @@ class MateriaController extends AbstractController
     
         // Obtener los últimos 5 cronómetros de la materia usando el nuevo método
         $timers = $cronometroRepository->findLastFiveByMateria($materium->getId());
+
+        $tareas = $tareaRepository->findAll();
+    
+        $ultimasTareas = $tareaRepository->findBy([], ['fecha' => 'DESC'], 5);
     
         // Preparar eventos para el calendario
         $events = [];
@@ -64,11 +68,35 @@ class MateriaController extends AbstractController
                 'start' => $tarea->getFecha()->format('Y-m-d'),
             ];
         }
-    
+
+        $inicioSemana = new \DateTime('monday this week');
+        $finSemana = new \DateTime('sunday this week');
+        $finSemana->setTime(23, 59, 59);
+
+        // Obtener las tareas de la semana actual
+        $tareasSemana = $tareaRepository->createQueryBuilder('t')
+            ->where('t.fecha >= :inicioSemana')
+            ->andWhere('t.fecha <= :finSemana')
+            ->setParameter('inicioSemana', $inicioSemana)
+            ->setParameter('finSemana', $finSemana)
+            ->getQuery()
+            ->getResult();
+
+        $events = [];
+        foreach ($tareas as $tarea) {
+            $events[] = [
+                'id' => $tarea->getId(),
+                'title' => $tarea->getTitulo(),
+                'start' => $tarea->getFecha()->format('Y-m-d'),
+            ];
+        }
+
         return $this->render('materia/show.html.twig', [
             'materium' => $materium,
             'events' => json_encode($events),
             'timers' => $timers, // Pasar los cronómetros a la vista
+            'ultimasTareas' => $ultimasTareas, 
+            'tareasSemana' => $tareasSemana,
         ]);
     }      
     
