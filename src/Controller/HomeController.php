dbac8a76b803530b7b8e3e5632265dc68e93f9dc
Tobi-Stani <stanislavskytobias@gmail.com>
2024-10-27 03:05:53 -0300

    mejorado el front del inicio

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CarreraRepository;
 use App\Repository\CuatrimestreRepository;
+use App\Repository\MateriaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,12 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(CarreraRepository $carreraRepository, CuatrimestreRepository $cuatrimestreRepository): Response
+    public function index(CarreraRepository $carreraRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'carreras' => $carreraRepository->findAll(),
-            'cuatrimestres' => $cuatrimestreRepository->findAll(),
         ]);
     }
 }
