1132b98c5a8cee46a840d14151f15d40c79e304f
Tobi-Stani <stanislavskytobias@gmail.com>
2024-10-27 22:17:43 -0300

    mejorado el front

--- src/Form/CuatrimestreType.php
+++ src/Form/CuatrimestreType.php
@@ -6,6 +6,7 @@ use App\Entity\Carrera;
 use App\Entity\Cuatrimestre;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +15,19 @@ class CuatrimestreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('numero')
-            ->add('anio')
+            ->add('numero', IntegerType::class, [
+                'label' => 'Número del Cuatrimestre',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('anio', IntegerType::class, [
+                'label' => 'Año',
+                'attr' => ['class' => 'form-control'],
+            ])
             ->add('carrera', EntityType::class, [
                 'class' => Carrera::class,
                 'choice_label' => 'nombre',
+                'label' => 'Carrera',
+                'attr' => ['class' => 'form-select'],
             ])
         ;
     }
