d14df25b806ab20a0b7dc6848639287bae2547ff
Tobi-Stani <stanislavskytobias@gmail.com>
2024-11-04 12:00:17 -0300

    redirecciona bien los botones

--- src/Form/CarreraType.php
+++ src/Form/CarreraType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Carrera;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,14 @@ class CarreraType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre')
-        ;
+        ->add('nombre', TypeTextType::class, [
+            'label' => 'Nombre de la carrera',
+            'attr' => [
+                'class' => 'form-control', 
+                'placeholder' => 'Ej: Licenciatura informatica', 
+                'autocomplete' => 'off'
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/MateriaType.php
+++ src/Form/MateriaType.php
@@ -9,6 +9,8 @@ use App\Entity\Tarea;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MateriaType extends AbstractType
@@ -16,17 +18,28 @@ class MateriaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre')
+            ->add('nombre', TextType::class, [
+                'label' => 'Nombre de la Materia',
+                'attr' => [
+                    'class' => 'form-control', // Clase de Bootstrap para el estilo
+                    'placeholder' => 'Escriba el nombre de la materia', // Placeholder descriptivo
+                    'autocomplete' => 'off',
+                ],
+            ])
             ->add('cuatrimestre', EntityType::class, [
                 'class' => Cuatrimestre::class,
                 'choice_label' => function (Cuatrimestre $cuatrimestre) {
                     $carrera = $cuatrimestre->getCarrera(); 
-                    
                     return $cuatrimestre->getNumero() . ' / ' . $cuatrimestre->getAnio() . 
                            ' (' . $carrera->getNombre() . ')'; 
                 },
-            ])
-        ;
+                'placeholder' => 'Seleccione el cuatrimestre', // Opción vacía para mejorar la usabilidad
+                'label' => 'Cuatrimestre',
+                'attr' => [
+                    'class' => 'form-select', // Bootstrap para selects
+                ],
+                'help' => 'Seleccione el cuatrimestre correspondiente a la materia.',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
